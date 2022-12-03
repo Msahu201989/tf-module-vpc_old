4ad45002eeea26414ade55796ddb1f957fe50464
Mukesh <msahu201989@gmail.com>
2022-12-03 19:46:30 +0530

    subnets

--- subnets/main.tf
+++ subnets/main.tf
@@ -1,6 +1,6 @@
 module "lm-subnets" {
-  for_each = var.subnets
-  subnets = each.value.cidr_block
-  source = "./lm-subnets"
-  vpc_id = var.vpc_id
+  for_each   = var.subnets
+  cidr_block = each.value.cidr_block
+  source     = "./lm-subnets"
+  vpc_id     = var.vpc_id[0]
 }
