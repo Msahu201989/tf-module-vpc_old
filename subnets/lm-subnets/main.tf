f63069efbe3991b647870c28e7680d4637c0335d
Mukesh <msahu201989@gmail.com>
2022-12-04 12:18:06 +0530

    subnets + Tag name + change Availability zone

--- subnets/lm-subnets/main.tf
+++ subnets/lm-subnets/main.tf
@@ -3,4 +3,5 @@ resource "aws_subnet" "main" {
   vpc_id     = var.vpc_id
   cidr_block = element(var.cidr_block, count.index)
   tags       = local.subnets_tags
+  availability_zone = element(var.subnet_availability_zones, count.index)
 }
