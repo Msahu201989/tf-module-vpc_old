a94c632809a2db1daa0bf2e6c32f68a3df803b91
Mukesh <msahu201989@gmail.com>
2022-12-09 21:13:37 +0530

    internet gw

--- subnets/main.tf
+++ subnets/main.tf
@@ -19,16 +19,16 @@
 #}
 
 resource "aws_subnet" "main" {
-  count      = length(var.cidr_block)
-  vpc_id     = var.vpc_id
-  cidr_block = element(var.cidr_block, count.index)
-  tags       = local.subnets_tags
+  count             = length(var.cidr_block)
+  vpc_id            = var.vpc_id
+  cidr_block        = element(var.cidr_block, count.index)
+  tags              = local.subnet_tags
   availability_zone = element(var.subnet_availability_zones, count.index)
 }
 
 resource "aws_route_table" "aws_route_table" {
-  vpc_id     = var.vpc_id
-  tags       = {
+  vpc_id = var.vpc_id
+  tags = {
     Name    = "${var.env}-${var.name}-rt"
     ENV     = var.env
     PROJECT = "roboshop"
