341db229af017aa70bab5d1b1db5d5a27aa1933b
Mukesh <msahu201989@gmail.com>
2022-12-09 13:26:37 +0530

    change vd 55 22min

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,8 @@
-output "private_subnets" {
-  value = module.private_subnets
-}
-
-output "public_subnets" {
-  value = module.public_subnets
-}
-
+#output "private_subnets" {
+#  value = module.private_subnets
+#}
+#
+#output "public_subnets" {
+#  value = module.public_subnets
+#}
+#
