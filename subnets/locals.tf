93926336099c085fbcf9f2c46bd420cbf82d3163
Mukesh <msahu201989@gmail.com>
2022-12-09 19:02:00 +0530

    subnets creation  vd 55 22min

--- subnets/locals.tf
+++ subnets/locals.tf
@@ -1,12 +1,27 @@
 locals {
-  vpc_tags  = {
-    Name    = "${var.env}-vpc"
-    ENV     = var.env
-    PROJECT = "roboshop"
-  }
-  vpc_peering_tags = {
-    Name    = "${var.env}-vpc-to-management-vpc"
-    ENV     = var.env
-    PROJECT = "roboshop"
+  subnets_tags  = {
+    Name        = "${var.name}-subnet"
+    ENV         = var.env
+    PROJECT     = "roboshop"
   }
 }
+
+
+
+
+
+
+
+
+#locals {
+#  vpc_tags  = {
+#    Name    = "${var.env}-vpc"
+#    ENV     = var.env
+#    PROJECT = "roboshop"
+#  }
+#  vpc_peering_tags = {
+#    Name    = "${var.env}-vpc-to-management-vpc"
+#    ENV     = var.env
+#    PROJECT = "roboshop"
+#  }
+#}
