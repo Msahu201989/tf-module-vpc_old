3d8279e0cee61217003aa00e25f368b425340a0f
Mukesh <msahu201989@gmail.com>
2022-12-09 18:54:24 +0530

    subnets creation  vd 55 22min

--- subnets/locals.tf
+++ subnets/locals.tf
@@ -1,1 +1,12 @@
-locals {}
+locals {
+  vpc_tags  = {
+    Name    = "${var.env}-vpc"
+    ENV     = var.env
+    PROJECT = "roboshop"
+  }
+  vpc_peering_tags = {
+    Name    = "${var.env}-vpc-to-management-vpc"
+    ENV     = var.env
+    PROJECT = "roboshop"
+  }
+}
--- subnets/vars.tf
+++ subnets/vars.tf
@@ -1,7 +1,14 @@
-variable "subnets" {}
+variable "cidr_block" {}
 variable "vpc_id" {}
 variable "env" {}
+variable "name" {}
 variable "subnet_availability_zones" {}
-variable "peering_connection_id" {}
-variable "management_vpc" {}
-variable "internet_gateway_id" {}
+
+
+#variable "subnets" {}
+#variable "vpc_id" {}
+#variable "env" {}
+#variable "subnet_availability_zones" {}
+#variable "peering_connection_id" {}
+#variable "management_vpc" {}
+#variable "internet_gateway_id" {}
