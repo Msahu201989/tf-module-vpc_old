3df8548a615afdf117b4b96158e010c0cbefba06
Mukesh <msahu201989@gmail.com>
2022-12-09 22:42:01 +0530

    change

--- outputs.tf
+++ outputs.tf
@@ -12,6 +12,6 @@ output "all_route_tables" {
 }
 
 
-#output "test" {
-#  value = lookup(lookup(module.public_subnets, "public", null), "subnets", null)[0].id
-#}
+output "test" {
+  value = lookup(lookup(module.public_subnets, "public", null), "subnets", null)[0].id
+}
