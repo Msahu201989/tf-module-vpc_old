9925aad0de33d5a72b6a180b13cb029c28ab710a
Mukesh <msahu201989@gmail.com>
2022-12-09 21:16:05 +0530

    change

--- subnets/main.tf
+++ subnets/main.tf
@@ -22,7 +22,7 @@ resource "aws_subnet" "main" {
   count             = length(var.cidr_block)
   vpc_id            = var.vpc_id
   cidr_block        = element(var.cidr_block, count.index)
-  tags              = local.subnet_tags
+  tags              = local.subnets_tags
   availability_zone = element(var.subnet_availability_zones, count.index)
 }
 
