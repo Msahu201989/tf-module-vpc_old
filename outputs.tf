ddb0c39aa30f7ce969afcb89719f1cfd6aa5a3c3
Cloud User <centos@ip-172-31-88-133.ec2.internal>
2022-12-10 13:14:41 +0000

    ss

--- outputs.tf
+++ outputs.tf
@@ -1,16 +1,16 @@
-output "private_subnets" {
-  value = module.private_subnets
-}
-
-output "public_subnets" {
-  value = module.public_subnets
-}
-
-
-output "all_route_tables" {
-  value = "local.all_route_tables"
-}
-
+#output "private_subnets" {
+#  value = module.private_subnets
+#}
+#
+#output "public_subnets" {
+#  value = module.public_subnets
+#}
+#
+#
+#output "all_route_tables" {
+#  value = "local.all_route_tables"
+#}
+#
 
 #output "test" {
 #  value = lookup(lookup(module.public_subnets, "public", null), "subnets", null)[0].id
