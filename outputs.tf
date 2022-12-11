95cddab29e3ac50e0cdc8426a2aa0070fc82a298
Mukesh <msahu201989@gmail.com>
2022-12-11 19:54:14 +0530

    security greoip

--- outputs.tf
+++ outputs.tf
@@ -12,6 +12,10 @@ output "all_route_tables" {
 }
 
 
-#output "test" {
-#  value = lookup(lookup(module.public_subnets, "public", null), "subnets", null)[0].id
-#}
+output "test" {
+  value = lookup(lookup(module.public_subnets, "public", null), "subnets", null)[0].id
+}
+
+output "vpc_id" {
+  value = "aws_vpc.main.id"
+}
